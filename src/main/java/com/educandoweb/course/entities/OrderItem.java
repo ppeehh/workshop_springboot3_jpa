d3ec841f3ee488988142a01494ba6b8136b809f4
Pedro Henrique <peehhenriquee18@gmail.com>
2024-09-04 15:58:46 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -3,7 +3,6 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -31,7 +30,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
-	@JsonIgnore
+	
 	public Order getOrder() {
 		return id.getOrder();
 	}
